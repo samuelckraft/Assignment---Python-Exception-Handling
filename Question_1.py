04e433a6797865a096d4c3cd5b01bb09dbd6f94f
samuelckraft <samuelckraft@gmail.com>
2024-04-25 13:14:09 -0400

    update

--- Question_1.py
+++ Question_1.py
@@ -1,1 +1,17 @@
 #Task 1
+try:
+    f_temp = int(input("Enter the temperature in farenheit: "))
+
+except ValueError:
+    print("Please input a numerical value")
+
+#Task 2/3
+try:
+    c_temp = (f_temp - 32) *(5/9)
+except (ValueError, ZeroDivisionError, OverflowError):
+    pass
+else:
+    print(f"It is {f_temp} degrees farenheit and {c_temp} degrees celsius")
+
+finally:
+    print("Thanks for using our weather forecaster")
